8c12c3b1adb53bac70192f93c53131807bab4977
Anuj <anujguptaaj123@gmail.com>
2025-08-25 21:19:08 +0530

    firebase config user data saved to db

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router'
 import Footer from '../components/footer'
 import Header from '@/components/header'
+import AuthHandler from '@/handler/user-auth-handler'
 
 const PublicLayout = () => {
   return (
     <div className='w-full '>
+      <AuthHandler/>
         <Header/>
         <Outlet/>
         <Footer/>
